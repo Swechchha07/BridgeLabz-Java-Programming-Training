32f001a6310d716bfe03132d63b5eaa807f2925a
swechchha <nigam316@gmail.com>
2026-05-05 10:46:08 +0530

    Factors of num using while

---
+++ CoreProgrammingIntermediate/ControlFlow/Level2/FactorsUsingWhile.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class FactorsUsingWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            System.out.println("Factors of " + number + " are:");
+            int i=1;
+            while (i < number) {
+                if (number % i == 0) {
+                    System.out.print(i + " ");
+                }
+                i++;
+            }
+            System.out.print(number);
+        }
+    }
+}
