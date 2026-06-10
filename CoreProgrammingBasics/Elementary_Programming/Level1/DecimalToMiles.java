32e9927bd287dd6f480270f83bef46a97a168146
swechchha <nigam316@gmail.com>
2026-06-10 19:12:48 +0530

    Questions on Oops Concept

--- CoreProgrammingBasics/Elementary_Programming/Level1/DecimalToMiles.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class DecimalToMiles {
-    public static void main(String[] args) {
-        double km=10.8;
-        double miles=km*1.6;
-        System.out.println("The distance "+ km + "km in miles is " + miles );
-
-    }
-}
--- CoreProgrammingBasics/Elementary_Programming/Level1/VolumeOfEarth.java
+++
@@ -1,8 +0,0 @@
-public class VolumeOfEarth {
-    public static void main(String[] args) {
-        int radius=6378;
-        double volumeKm=(4/3)*Math.PI*(radius^3);
-        double volumeMiles = volumeKm * Math.pow(0.621371, 3);
-        System.out.println("The volume of earth in cubic kilometers is " + volumeKm + " and cubic miles is " + volumeMiles);
-    }
-}
