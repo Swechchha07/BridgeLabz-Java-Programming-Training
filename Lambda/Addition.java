f3c3d055bf3532317f2b482f907941483a549579
Swechchha Nigam <swechchha.nigam_cs22@gla.ac.in>
2026-06-17 10:59:37 +0530

    add using lambda expression

---
+++ Lambda/Addition.java
@@ -0,0 +1,12 @@
+@FunctionalInterface
+interface Calculator {
+    int add(int a,int b);
+    
+}
+
+public class Addition {
+    public static void main(String[] args) {
+        Calculator c=(a,b)->a+b;
+        System.out.println(c.add(10,20));
+    }
+}
